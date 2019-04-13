df10f825ceeec669f50a24e76cfc776ba9375879
Mark <mharmsenr@gmail.com>
2019-04-13 13:46:29 -0300

    Add filtered data to the UI

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,7 +1,25 @@
 function data(state, action) {
     switch (action.type) {
-        case '': {
-            return state
+        case 'SEARCH_VIDEO': {
+            // action.payload.query
+            let results = []
+            if(action.payload.query) {
+                const categories = state.data.categories;
+                categories.map(category => {
+                    let tempResults = category.playlist.filter(item => {
+                        const itemAuthor = item.author.toLowerCase();
+                        const payloadQuery = action.payload.query.toLowerCase();
+        
+                        return itemAuthor.includes(payloadQuery);
+                    })
+                    results = results.concat(tempResults);
+                })
+            }
+
+            return {
+                ...state,
+                search: results
+            }
         }
         default:
             return state 
